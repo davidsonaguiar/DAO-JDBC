1ec59364b1d7054a50b4e8ebe1aeaacb3d609777
Davidson <davidson.05@outlook.com>
2024-06-16 15:18:57 -0300

    add seller entity

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         try {
             DB.getConnection();
             System.out.println("Connection opened!");
+
             var department = new Department(1, "Books");
             System.out.println(department);
+
+            var seller = new Seller(21, "Bob", "bob@gmail.com", 3000.0, new Date(), department);
+            System.out.println(seller);
         }
         catch (Exception e) {
             System.out.println("Error accessing database!");
