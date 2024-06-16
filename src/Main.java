08294a52a9c2d947968952040425476bc706e4e1
Davidson <davidson.05@outlook.com>
2024-06-16 16:14:44 -0300

    Seller add FindById

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,17 @@
-import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            DB.getConnection();
-            System.out.println("Connection opened!");
-
-            var department = new Department(1, "Books");
-            System.out.println(department);
-
-            var seller = new Seller(21, "Bob", "bob@gmail.com", 3000.0, new Date(), department);
-            System.out.println(seller);
-
             SellerDao sellerDao = DaoFactory.createSellerDao();
+            Seller seller = sellerDao.findById(3);
+            System.out.println(seller);
         }
         catch (Exception e) {
-            System.out.println("Error accessing database!");
             System.out.println(e.getMessage());
-        }
-        finally {
-            System.out.println("Closing connection!");
-            DB.closeConnection();
         }
     }
 }
--- src/model/implementation/SellerDaoJDBC.java
+++ src/model/implementation/SellerDaoJDBC.java
@@ -1,11 +1,25 @@
 package model.implementation;
 
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private final Connection connection;
+
+    public SellerDaoJDBC(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public void insert(Seller obj) {
 
@@ -23,7 +37,42 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        String sql = "SELECT seller.*, department.Name as DepName FROM seller INNER JOIN department ON seller.DepartmentId = department.Id WHERE seller.Id = ?";
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+            if (!resultSet.next()) return null;
+
+            Integer departmentId = resultSet.getInt("DepartmentId");
+            String departmentName = resultSet.getString("DepName");
+            Department department = new Department(departmentId, departmentName);
+
+            Integer sellerId = resultSet.getInt("Id");
+            String sellerName = resultSet.getString("Name");
+            String sellerEmail = resultSet.getString("Email");
+            Double sellerBaseSalary = resultSet.getDouble("BaseSalary");
+            Date sellerBirthDate = resultSet.getDate("BirthDate");
+            Seller seller = new Seller(sellerId, sellerName, sellerEmail, sellerBaseSalary, sellerBirthDate, department);
+
+            return seller;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            try {
+                if (preparedStatement != null) preparedStatement.close();
+                if (resultSet != null) resultSet.close();
+            }
+            catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
     }
 
     @Override
